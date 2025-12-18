07791b2c3c4bfd3b5f37ac23afebc454632c688d
DungNQ <dungnq@DungNQs-Mac-mini.local>
2025-12-18 17:26:18 +0700

    feat: header

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ecomobile-server.github.io',
-  base: '/EPM-landing-page',
+  site: 'https://epm.ecomobile.vn',
 });
